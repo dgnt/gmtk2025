248b2bfd957edc472e94efe83bff8e62a01b9360
Tim Meyer <dgnt22@gmail.com>
2025-07-30 12:03:23 -0700

    basic hula hoop motion prototype

--- test/scripts/player/player_body.gd
+++ test/scripts/player/player_body.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 300.0
+@export var speed = 100.0
 @export var jump_velocity = -400.0
-
+const REV_TIME = 800  # ms
+const HOOP_SPEED = 90
+var rev = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		rev += delta * 1000 / REV_TIME * 2 * PI
+		while rev > 2 * PI:
+			rev -= 2 * PI
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -17,8 +24,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction * speed + sin(rev) * HOOP_SPEED 
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, sin(rev) * HOOP_SPEED, speed)
 
 	move_and_slide()
