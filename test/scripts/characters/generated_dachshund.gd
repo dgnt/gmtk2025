ea155a6ae6de63a8ca2db296ab825a12ddeca5ea
Alex Wilson <awlego@gmail.com>
2025-08-01 16:34:42 -0700

    Adding the full char

--- test/scripts/characters/generated_dachshund.gd
+++ test/scripts/characters/generated_dachshund.gd
@@ -1,43 +1,229 @@
 extends CharacterBody2D
+signal level_failed
 
 @export var speed = 300.0
-@export var jump_velocity = -400.0
+@export var jump_velocity = -600.0
 
 @onready var animation_player = $AnimationPlayer
 @onready var skeleton = $Skeleton2D
+@onready var facing_forward = true
 
-func _ready():
+const REV_TIME = 800  # ms
+const HOOP_SPEED = 300
+const STABILITY = 250
+var rev = 200
+var lookup = false
+var hypercharge = 0
+const CHARGE_TIME = 2000.0 # ms
+const FULL_CHARGE = 2.0
+var locked_skills = []
+const SKILLS = ["jump", "move_left", "move_right", "move_up", "move_down"]
+const HEIGHT = 256 #px
+const WIDTH = 64 #px
+const WAIST = 40
+const SHOULDER = 190
+const HEAD = 206
+const TOP_HEAD = 251
+const THICK_BODY = 35
+const THICK_HEAD = 45
+
+const HEAD_NAME = "Head"
+const TORSO_NAME = "Torso"
+const BELLY_NAME = "Belly"
+const BODY_NAME = "Body"
+var start_pos = Vector2(100,100)
+
+		
+func _physics_process(delta: float) -> void:
+	var bod = get_node("Body")
+	var pressed = []
+	for action in SKILLS:
+		if action not in locked_skills and Input.is_action_pressed(action):
+			pressed.append(action)
+		
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		if "move_down" in pressed:
+			hypercharge += delta * 1000
+			if hypercharge > CHARGE_TIME:
+				hypercharge = CHARGE_TIME
+		else:
+			hypercharge = 0
+		rev += delta * 1000 / REV_TIME * (1+hypercharge/CHARGE_TIME*FULL_CHARGE)
+		rev -= int(rev)
+		lookup = "move_up" in pressed
+		$Path2D.rotation = 0 if not lookup else bod.transform.x.x * -45
+	
+	var hooper = get_node("Path2D/PathFollow2D")
+	hooper.progress_ratio = rev
+	
+	# Handle jump.
+	if "jump" in pressed and is_on_floor():
+		velocity.y = jump_velocity
+		if lookup:
+			velocity.y += cos(rev * 2 * PI) * HOOP_SPEED * bod.transform.x.x
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	var hoop_h = 0
+	if not lookup:
+		hoop_h = -cos(rev*2*PI) * HOOP_SPEED * (1 + hypercharge / CHARGE_TIME * FULL_CHARGE)
+		if is_on_floor():
+			if abs(hoop_h) < STABILITY or hypercharge > 0:
+				hoop_h = 0
+			elif hoop_h > 0:
+				hoop_h -= STABILITY
+			else:
+				hoop_h += STABILITY
+	if direction and "move_left" not in locked_skills and "move_right" not in locked_skills:
+		velocity.x = direction * speed + hoop_h
+	else:
+		velocity.x = move_toward(velocity.x, hoop_h, speed)
+	
+	if direction:
+		if velocity.x < 0:
+			bod.transform.x = Vector2(-1, 0)
+		elif velocity.x > 0:
+			bod.transform.x = Vector2(1, 0)
+	move_and_slide()
+	
+	for i in range(get_slide_collision_count()):
+		var collider = get_slide_collision(i).get_collider()
+		if collider.name == "Spikes":
+			level_failed.emit()
+			set_deferred("monitoring", false) # Disable monitoring after first trigger
+			set_deferred("process_mode", Node.PROCESS_MODE_DISABLED) # Disable script processing
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
 	if animation_player:
 		setup_animations()
 	
 	# If running scene in isolation, setup test environment
 	if get_tree().current_scene == self:
 		setup_test_environment()
-
-func _physics_process(delta):
-	# Add gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
-		if animation_player:
-			animation_player.play("jump")
-	
-	# Handle movement
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * speed
-		# Flip character
-		if direction < 0:
-			scale.x = -abs(scale.x)
-		else:
-			scale.x = abs(scale.x)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		locked_skills = get_parent().get_locked_skills()
 	
-	move_and_slide()
+	start_pos = position
+	# make_torso()
+	# make_head()
+	print("Hitbox size is ", get_hitbox_dimensions())
+	pass # Replace with function body.
+
+
+func sane_coord(point: Vector2) -> Vector2:
+	return Vector2(point.x, HEIGHT - point.y)
+
+func sanify_pack(v2a: PackedVector2Array) -> void:
+	for v2 in v2a:
+		v2.y = HEIGHT - v2.y
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D):
+	print("Collision!")
+	if body.get_collision_layer_bit(2):
+		position = start_pos
+	pass
+	
+func get_hitbox_dimensions():
+	var top = $CollisionPolygon2D.polygon[0].y
+	var bot = $CollisionPolygon2D.polygon[0].y
+	var left = $CollisionPolygon2D.polygon[0].x
+	var right = $CollisionPolygon2D.polygon[0].x
+	for point in $CollisionPolygon2D.polygon:
+		top = min(top, point.y)
+		bot = max(bot, point.y)
+		left = min(left, point.x)
+		right = max(right, point.x)
+	return Vector2($CollisionPolygon2D.scale.x*(bot-top), $CollisionPolygon2D.scale.y*(right-left))
+	
+func make_head() -> void:
+	var top = HEIGHT/2 - TOP_HEAD
+	var bot = HEIGHT/2 - HEAD
+	var left = -THICK_HEAD/2
+	var right = THICK_HEAD/2
+	
+	var head = Polygon2D.new()
+	head.name = HEAD_NAME
+	head.color = Color("#632b16")
+	head.polygon = PackedVector2Array([
+		Vector2(left,top),
+		Vector2(left,bot),
+		Vector2(right,bot),
+		Vector2(right,top)
+	])
+	get_node("Body").add_child(head)
+
+func make_torso() -> void:
+	var top = HEIGHT/2 - SHOULDER
+	var bot = HEIGHT/2 - WAIST
+	var left = -THICK_BODY/2
+	var right = THICK_BODY/2
+	
+	var torso = Polygon2D.new()
+	torso.name = TORSO_NAME
+	torso.color = Color("#632b16")
+	torso.polygon = PackedVector2Array([
+		Vector2(left,top),
+		Vector2(left,bot),
+		Vector2(right,bot),
+		Vector2(right,top)
+	])
+	#sanify_pack(torso.polygon)
+	torso.clip_children = Node2D.CLIP_CHILDREN_AND_DRAW
+	get_node("Body").add_child(torso)
+	
+	var belly = Polygon2D.new()
+	belly.name = BELLY_NAME
+	belly.color = Color("#6e4f3d")
+	belly.polygon = PackedVector2Array([
+		Vector2((left + right) * 0.6, (top + bot) * 0.3),
+		Vector2(right + 10, (top+bot) * 0.2),
+		Vector2(right + 10, bot + 10),
+		Vector2((left+right) * 0.5, bot - 10)
+	])
+	torso.add_child(belly)
+	
+	#self.get_node(TORSO_NAME).clip_children = Node2D.CLIP_CHILDREN_AND_DRAW
+
+
+func make_belly() -> void:
+	var top = -1000
+	var left = -1000
+	var right = 1000
+	var bot = 1000
+	var torso_points = PackedVector2Array([
+		Vector2(left, bot), Vector2(right, bot), Vector2(right, top), Vector2(left, top)
+	])
+	var shape_color = Color("#6e4f3d")
+	var torso = self.get_node(TORSO_NAME)
+
+func create_polygon_node(node_name: String, points: PackedVector2Array, shape_color: Color) -> Polygon2D:
+	# 1. Create a new Polygon2D instance in memory.
+	var new_polygon = Polygon2D.new()
+	
+	# 2. Set its properties.
+	new_polygon.name = node_name
+	new_polygon.polygon = points
+	new_polygon.color = shape_color
+	
+	# 3. Add it as a child to the specified parent node.
+	#    This makes it part of the active scene tree.
+	#parent_node.add_child(new_polygon)
+	
+	# 4. Return the new node instance in case you need a direct reference to it.
+	return new_polygon
+
 	
 func setup_animations():
 	# Find the AnimationPlayer
