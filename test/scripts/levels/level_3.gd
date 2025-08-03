acab48fb098d066ed4e2e480746642bd1ee51055
Brandon Robinson <brobinson@Brandons-MacBook-Pro.local>
2025-08-02 23:00:15 -0700

    new stage look for stage 3

--- test/scripts/levels/level_3.gd
+++ test/scripts/levels/level_3.gd
@@ -4,6 +4,11 @@ extends "res://scripts/levels/generic_level.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
+	$Dachshund.hypercharge_allowed = false
+	$Dachshund.helicopter_allowed = true
+	$Dachshund.snap_allowed = false
+	$Dachshund.hoop_control_allowed = true
+	$Dachshund.hoop_on = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
